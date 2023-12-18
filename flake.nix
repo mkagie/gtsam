c4e1b9e90d7341a468912f3f0dbba6716797c7e2
Matt Kagie <mjkagie@gmail.com>
2023-12-17 17:37:06 -0800

    Things work for python now

--- flake.nix
+++ flake.nix
@@ -70,20 +70,60 @@
         };
         devShells = {
           default = pkgs.mkShell {
-            inputsFrom = [ gtsam ];
+            inputsFrom = [ gtsam-py ];
             packages = with pkgs; [
               clang
               cmakeCurses
             ];
           };
-          python = (pkgs.buildFHSUserEnv {
-            name = "gtsam";
-            targetPkgs = pkgs: (with pkgs; [
+
+          python = pkgs.mkShell rec {
+            name = "gtsam-2";
+            inputsFrom = [ gtsam ];
+            buildInputs = with pkgs; [
+              libz
+            ];
+            packages = with pkgs; [
               python3
               python3.pkgs.pip
               python3.pkgs.virtualenv
-            ]);
-          }).env;
+            ];
+            shellHook = ''
+              VENV_DIR="venv"
+
+              # Check if the virtual environment exists
+              if [ -d "$VENV_DIR" ]; then
+                  echo "Activating existing virtual environment..."
+                  source "$VENV_DIR/bin/activate"
+              else
+                  echo "Creating virtual environment..."
+                  virtualenv "$VENV_DIR"
+
+                  # Check if the virtual environment creation was successful
+                  if [ $? -eq 0 ]; then
+                      echo "Virtual environment created successfully. Activating..."
+                      source "$VENV_DIR/bin/activate"
+
+                      echo "Installing required pacakges"
+                      pip install --upgrade pip setuptools
+                      pip install -r python/dev_requirements.txt
+                      pip install -r python/requirements.txt
+
+                      if [ -e "build/python/setup.py" ]; then
+                        echo "Installing gtsam"
+                        cd build && make python-install
+                        cd ..
+                      else
+                        echo "Error: No GTSAM to install -- try building it"
+                      fi
+                  else
+                      echo "Error: Failed to create virtual environment."
+                  fi
+              fi
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+            '';
+          };
         };
       }
     );
