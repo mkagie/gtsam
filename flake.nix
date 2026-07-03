d9964920a7fdf9c0587ddc29521d5d5b83e725e7
Matt Kagie <mjkagie@gmail.com>
2026-07-03 14:31:45 -0700

    Got things building again

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-26.05";
     };
     flake-utils = {
       url = "github:numtide/flake-utils";
